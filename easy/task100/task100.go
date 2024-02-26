ad94f823fe76aa0be5bed35bd86040bf1c82fa8c
MisterGnida <https.genius@gmail.com>
2024-02-26 16:51:50 +0300

    Added task100

--- easy/task100/task100.go
+++ easy/task100/task100.go
@@ -1,7 +1,45 @@
 package task100
 
-import "leetcode_solutions/internal/treenode"
+import (
+	"leetcode_solutions/internal/queue"
+	"leetcode_solutions/internal/treenode"
+)
 
-func isSameTree(p *treenode.TreeNode, q *treenode.TreeNode) {
+func IsSameTree(p *treenode.TreeNode, q *treenode.TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
 
+	var queue = queue.New()
+	queue.PushLast(p)
+	queue.PushLast(q)
+
+	for queue.Head() != nil {
+		pNode := queue.PopFirst().(*treenode.TreeNode)
+		qNode := queue.PopFirst().(*treenode.TreeNode)
+
+		if pNode.Val != qNode.Val {
+			return false
+		}
+
+		if pNode.Left != nil && qNode.Left != nil {
+			queue.PushLast(pNode.Left)
+			queue.PushLast(qNode.Left)
+		} else if pNode.Left != nil || qNode.Left != nil {
+			return false
+		}
+
+		if pNode.Right != nil && qNode.Right != nil {
+			queue.PushLast(pNode.Right)
+			queue.PushLast(qNode.Right)
+		} else if pNode.Right != nil || qNode.Right != nil {
+			return false
+		}
+		queue.PrintQueue()
+	}
+
+	return true
 }

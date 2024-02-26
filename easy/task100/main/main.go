11836c0f04043896d842d899d1953d4ba71bc150
MisterGnida <https.genius@gmail.com>
2024-02-26 16:52:34 +0300

    Added `main` for task100

--- easy/task100/main/main.go
+++ easy/task100/main/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"leetcode_solutions/easy/task100"
+	"leetcode_solutions/internal/treenode"
+)
+
 func main() {
+	arr1 := []interface{}{1, 2, 3, 5, 6, 7, 8, nil, nil, 10}
+	arr2 := []interface{}{1, 2, 3, 5, 6, 2, 8, nil, nil, 10}
+	p := treenode.FillTree(arr1)
+	q := treenode.FillTree(arr2)
+	fmt.Println(task100.IsSameTree(p, q))
 
 }
